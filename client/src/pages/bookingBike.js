23c04723f8d9742688d5f99eda0f47d8a91330bb
sabbir-hossain-abir <sabbir.abirs99@gmail.com>
2022-12-11 00:08:50 +0600

    added pages

---
+++ client/src/pages/bookingBike.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function bookingBike() {
+    return ( 
+        <div>
+            <h1>Booking Bike Page</h1>
+        </div>
+     );
+}
+
+export default bookingBike;
