8f69712ffb60d49a7492e53b964135a76af555e7
ZakariaBinMoti <zakaria.binmoti@gmail.com>
2022-12-20 21:17:36 +0600

    sign up and sign in

--- client/src/components/DefaultLayout.js
+++ client/src/components/DefaultLayout.js
@@ -1,19 +1,73 @@
-import React, { Fragment } from "react";
+import React from "react";
+import { Menu, Dropdown, Button, Space , Row , Col } from "antd";
+import {Link} from 'react-router-dom'
 
-import Header from "../components/Header";
-import Footer from "../components/Footer";
-import Routers from "../components/Routers";
-
-const Layout = () => {
-  return (
-    <Fragment>
-      <Header />
-      <div>
-        <Routers />
-      </div>
-      <Footer />
-    </Fragment>
+function DefaultLayout(props) {
+    const user = JSON.parse(localStorage.getItem('user'))
+  const menu = (
+    <Menu>
+        <Menu.Item>
+        <a
+         
+          href="/"
+        >
+          Home
+        </a>
+      </Menu.Item>
+      <Menu.Item>
+        <a
+          
+          href="/userbookings"
+        >
+          Bookings
+        </a>
+      </Menu.Item>
+      <Menu.Item>
+        <a
+         
+          href="/admin"
+        >
+          Admin
+        </a>
+      </Menu.Item>
+      <Menu.Item onClick={()=>{
+          localStorage.removeItem('user');
+          window.location.href='/login'
+      }}>
+          <li style={{color:'orangered'}}>Logout</li>
+      </Menu.Item>
+    </Menu>
   );
-};
+  return (
+    <div>
+      <div className="header bs1">
+          <Row gutter={16} justify='center'>
+              <Col lg={20} sm={24} xs={24}>
+              <div className="d-flex justify-content-between">
+             <h1 ><b><Link to='/' style={{color:'orangered'}}>SheyCars</Link></b></h1>
 
-export default Layout;
+          <Dropdown overlay={menu} placement="bottomCenter">
+            <Button>{user.username}</Button>
+          </Dropdown>
+        </div>
+              </Col>
+          </Row>
+        
+      </div>
+      <div className="content">{props.children}</div>
+
+      <div className="footer text-center">
+      <hr />
+
+           <p>Desinged and Developed By</p>
+
+           
+
+           <p>SHEY</p>
+          
+      </div>
+    </div>
+  );
+}
+
+export default DefaultLayout;
